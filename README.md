b0a059b3103ec89d1cf6fe8e2164859d1ccb6b19
davidcastro96 <davidcastro0796@outlook.es>
2021-01-24 11:43:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Status
+
+## Download and Installation
+
+## Usage
+
+### Basic Usage
---
+++ js/main.js
@@ -0,0 +1,26 @@
+var ourRequest = new XMLHttpRequest();
+ourRequest.open(
+  "GET",
+  "https://raw.githubusercontent.com/davidcastro96/heroes-app/main/heroes.json?token=ALOFPYUO4N2RHLKZX7AFE3LABRPSO"
+);
+ourRequest.onload = function () {
+  var ourDate = JSON.parse(ourRequest.responseText);
+  console.log(ourDate.heroes[2]);
+};
+ourRequest.send();
+
+/* este es el main que creaste */
+var votepositive = 30;
+var votenegative = 18;
+
+function voteLike() {
+  votepositive += 1;
+  result = "Tu voto <br/>ha sido registrado";
+  document.getElementById("result").innerHTML = votepositive;
+  document.getElementById("res").innerHTML = result;
+}
+
+function voteDislike() {
+  votenegative += 1;
+  document.getElementById("result").innerHTML = votenegative;
+}
